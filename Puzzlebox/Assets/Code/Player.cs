23bb42f746f6cc2c7f453f2f47d298fef15b794e
Mugiten <makoto_singleton@hotmail.com>
2019-05-27 19:32:16 -0700

    SlowTime Revised
    + SlowTime works as intended. Once the x key is released, time will move on normal speeds, and stamina will increase.

--- Puzzlebox/Assets/Code/Player.cs
+++ Puzzlebox/Assets/Code/Player.cs
@@ -17,6 +17,9 @@ public class Player : MonoBehaviour
     private Vector3 bottomLeftLimit;
     private Vector3 topRightLimit;
 
+    // Created a usingStamina bool
+    private bool usingStamina = false;
+
     public bool canMove = true;
 
     void Start()
@@ -74,22 +77,36 @@ public class Player : MonoBehaviour
             
     }
 
+    /*
+     When you push on the X key, it will decrement the player stamina, and change the bool usingStamina to true. Time will slow down, until the 
+     X key is released, then the stamina will increment, and time will change back to the normal speed. 
+         */
     public void SlowTime() {
+        usingStamina = false;
+
         if (Input.GetKey("x"))
         {
-            if (PlayerStats.instance.playerStamina > 0)
+            PlayerStats.instance.playerStamina--;
+            if (PlayerStats.instance.playerStamina > 0F)
             {
-                Time.timeScale = 0.1F;
-                Time.fixedDeltaTime = 0.02F * Time.timeScale;
-                PlayerStats.instance.playerStamina--;
+                usingStamina = true;
             }
+        }
+        else {
+            if (PlayerStats.instance.playerStamina < PlayerStats.instance.fullStamina)
+            {
+                PlayerStats.instance.playerStamina++;
+            }
+        }
+
+        if (usingStamina)
+        {
+            Time.timeScale = 0.1F;
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
         }
         else {
             Time.timeScale = 1;
             Time.fixedDeltaTime = 0.02F * Time.timeScale;
-            if (PlayerStats.instance.playerStamina < PlayerStats.instance.fullStamina) {
-                PlayerStats.instance.playerStamina++;
-            }
         }
     }
 }
