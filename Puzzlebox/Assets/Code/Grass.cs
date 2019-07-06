9819a67ebde221623da2e0d3b9464158ef55cfc3
Mugiten <makoto_singleton@hotmail.com>
2019-07-05 17:53:39 -0700

    Created a Destroyable Grass Object
    +The grass object is destroyable by the attack key (c).
    +It animated properly, and turns to leaves when hit.

---
+++ Puzzlebox/Assets/Code/Grass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Erase() {
+        anim.SetBool("ifHit", true);
+    }
+}
