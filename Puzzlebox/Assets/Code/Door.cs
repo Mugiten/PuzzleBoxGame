9a35c6370a3b6f1b3302505338baa237b621e6c6
Mugiten <makoto_singleton@hotmail.com>
2019-12-29 15:48:25 -0800

    Door2
    +Still trying to implement key and door. Also considering implementing other kinds of doors like switches.

--- Puzzlebox/Assets/Code/Door.cs
+++ Puzzlebox/Assets/Code/Door.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// There will be multiple kinds of doors. Creating an enum for the types of doors. 
+public enum DoorType {
+    key,
+    enemy,
+    button
+}
+
+
+
 public class Door : Interactable
 {
+    // Door Variables. 
     public bool hasKey;
     public SpriteRenderer doorSprite;
+    public DoorType thisDoorType;
+    public bool open = false; 
+
+    
     // Start is called before the first frame update
     void Start(){
         
@@ -17,7 +31,4 @@ public class Door : Interactable
             doorSprite.enabled = false;
         }
     }
-
-
-
 }
