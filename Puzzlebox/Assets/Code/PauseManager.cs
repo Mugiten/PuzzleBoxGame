da189f1b1eb3b0da5fad9fb1e5d55282955c0489
Mugiten <makoto_singleton@hotmail.com>
2020-01-04 17:04:54 -0500

    Making Inventory p2
    +Created a pause menu.
    +Using prebuilt UI. Will make it functionable soon.

---
+++ Puzzlebox/Assets/Code/PauseManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isPaused;
+    public GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        isPaused = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changePause();
+    }
+
+    void changePause() {
+        if (Input.GetButtonDown("Pause")) {
+            isPaused = !isPaused;
+            if (isPaused) {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else {
+                pausePanel.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    public void Resume() {
+        isPaused = !isPaused;
+    }
+}
