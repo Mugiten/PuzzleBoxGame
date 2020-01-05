edb8d198a2656445d23f829e48944c309569ad44
Mugiten <makoto_singleton@hotmail.com>
2020-01-05 18:08:41 -0500

    Making Inventory p3
    +Making Inventory Manger.

---
+++ Puzzlebox/Assets/Code/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/Player Inventory", fileName = "New Player Inventory")]
+public class Inventory : ScriptableObject
+{
+    public List<InventoryItem> myInventory = new List<InventoryItem>();
+
+    public void AddItem(InventoryItem newItem) {
+        if (!myInventory.Contains(newItem))
+            myInventory.Add(newItem);
+    }
+
+    public void RemoveItem(InventoryItem newItem) {
+        if (myInventory.Contains(newItem))
+            myInventory.Remove(newItem);
+    }
+
+    public bool isItemInInventory(InventoryItem newItem) {
+        return myInventory.Contains(newItem);
+    }
+
+}
---
+++ Puzzlebox/Assets/Code/InventoryManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    [Header("Inventory Information")]
+    [SerializeField] private GameObject blankInventorySlot;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private Text descriptionText;
+    [SerializeField] private GameObject useButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
