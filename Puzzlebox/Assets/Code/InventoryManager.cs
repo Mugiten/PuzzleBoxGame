14faf1cef1dd015534e94ec3972843f21194a555
Mugiten <makoto_singleton@hotmail.com>
2020-01-09 22:10:35 -0500

    Invetory/Pause Menu p5
    +Got the description to show up.

--- Puzzlebox/Assets/Code/InventoryManager.cs
+++ Puzzlebox/Assets/Code/InventoryManager.cs
@@ -7,20 +7,31 @@ public class InventoryManager : MonoBehaviour
 {
 
     [Header("Inventory Information")]
-    [SerializeField] private GameObject blankInventorySlot;
-    [SerializeField] private GameObject inventoryPanel;
+    //[SerializeField] private GameObject blankInventorySlot;
+    //[SerializeField] private GameObject inventoryPanel;
     [SerializeField] private Text descriptionText;
     [SerializeField] private GameObject useButton;
+    [SerializeField] private GameObject itemObject;
+    [SerializeField] private Button currentButton;
+    [SerializeField] private InventoryItem currentItem;
+
+    public static InventoryManager instance; 
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        showDescription();
+    }
+
+    public void showDescription() {
+        descriptionText.text = currentItem.itemDescription;
     }
 }
