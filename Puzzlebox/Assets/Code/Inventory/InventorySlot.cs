2fedb3ea03ff275232ad675f17fbebd8e138a87c
Mugiten <makoto_singleton@hotmail.com>
2020-01-12 16:40:03 -0500

    Inventory p6
    +Inventory works for now, there does appear to be a problem with how the Content Canvas and the empty slots draws in the viewport. I had to change the content size manually, but that's not going to be a premanent solution.

---
+++ Puzzlebox/Assets/Code/Inventory/InventorySlot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [Header("UI Stuff")]
+    [SerializeField] private Text itemName; 
+    [SerializeField] private Image itemImage;
+
+    [Header("Variable from the item")]
+    public Sprite itemSprite;
+    public Text itemDescription;
+    public InventoryItem thisItem;
+    public InventoryManager thisManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager) {
+        thisItem = newItem;
+        thisManager = newManager;
+        if (thisItem) {
+            itemImage.sprite = thisItem.itemImage;
+            itemDescription.text = thisItem.itemDescription;
+        }
+    }
+
+    public void ClickedOn() {
+        if (thisItem) {
+            thisManager.SetTextAndButton(thisItem.itemDescription, thisItem.usable);
+        }
+    }
+}
